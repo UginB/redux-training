372c3da092c62254531d995915570ce0cedaf8fb
Evgeniy Bratenkin <ugin-bro@ya.ru>
2022-01-05 18:25:10 +0300

    first commit

---
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -0,0 +1,77 @@
+// Задача для этого компонента:
+// Реализовать создание нового героя с введенными данными. Он должен попадать
+// в общее состояние и отображаться в списке + фильтроваться
+// Уникальный идентификатор персонажа можно сгенерировать через uiid
+// Усложненная задача:
+// Персонаж создается и в файле json при помощи метода POST
+// Дополнительно:
+// Элементы <option></option> желательно сформировать на базе
+// данных из фильтров
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHttp } from '../../hooks/http.hook';
+
+const HeroesAddForm = () => {
+    const {newHero, setNewHero} = useState({});
+    const {request} = useHttp();
+    const {heroes, heroesLoadingStatus} = useSelector(state => state);
+    const dispatch = useDispatch();
+    
+    const onVlueChange = (e) => {
+        console.log(e.target);
+        // setNewHero({...newHero, e.target.value})
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        dispatch({type: 'HERO_ADD', payload: {}})
+    }
+
+    return (
+        <form className="border p-4 shadow-lg rounded">
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+                <input 
+                    required
+                    type="text" 
+                    name="name" 
+                    className="form-control" 
+                    id="name" 
+                    placeholder="Как меня зовут?"
+                    onChange={onVlueChange}/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="text" className="form-label fs-4">Описание</label>
+                <textarea
+                    required
+                    name="text" 
+                    className="form-control" 
+                    id="text" 
+                    placeholder="Что я умею?"
+                    style={{"height": '130px'}}
+                    onChange={onVlueChange}/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+                <select 
+                    required
+                    className="form-select" 
+                    id="element" 
+                    name="element"
+                    onChange={onVlueChange}>
+                    <option >Я владею элементом...</option>
+                    <option value="fire">Огонь</option>
+                    <option value="water">Вода</option>
+                    <option value="wind">Ветер</option>
+                    <option value="earth">Земля</option>
+                </select>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Создать</button>
+        </form>
+    )
+}
+
+export default HeroesAddForm;
